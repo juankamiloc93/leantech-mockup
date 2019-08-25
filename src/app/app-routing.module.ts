27723c5d603c0fd42dcfb3a98aefface92b3c1aa
Juan Camilo <juankamiloc93@gmail.com>
2019-08-25 15:47:17 -0500

    inited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/views/home/home.component';
+import { MessagesComponent } from './components/views/messages/messages.component';
+import { WishlistComponent } from './components/views/wishlist/wishlist.component';
+import { SettingsComponent } from './components/views/settings/settings.component';
+import { AccountComponent } from './components/views/account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'wishlist', component: WishlistComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'account', component: AccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { MessagesComponent } from './components/views/messages/messages.component';
+import { WishlistComponent } from './components/views/wishlist/wishlist.component';
+import { SettingsComponent } from './components/views/settings/settings.component';
+import { AccountComponent } from './components/views/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessagesComponent,
+    WishlistComponent,
+    SettingsComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
